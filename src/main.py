ddd37458e2297686a08c24ab41201131ff6c637e
Leônidas Araújo <leonidasaraujomqm@gmail.com>
2025-08-30 16:44:57 -0300

    implement game class and update home route

--- src/main.py
+++ src/main.py
@@ -3,6 +3,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+class Game:
+    def __init__(self, name, category, console):
+        self.name = name
+        self.category = category
+        self.console = console
+
+
 @app.route('/')
 def home():
-    return render_template('list.html')
+    game1 = Game('God of War', 'Ação', 'PlayStation')
+    game2 = Game('Skyrim', 'RPG', 'PC')
+    game3 = Game('Valorant', 'Tiro', 'PC')
+    game_list = [game1, game2, game3]
+    return render_template('list.html', title='Jogos', game_list=game_list)
